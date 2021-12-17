2ecbaf3cfba3d6ed3be03b1764dd08d350ddc372
wzy0000 <95964271+wzy0000@users.noreply.github.com>
2021-12-17 21:36:14 +0800

    Add files via upload

--- dataset_csv.py
+++ dataset_csv.py
@@ -3,8 +3,9 @@ import pandas as pd
 import os
 import csv
 import random
+import queue
 
-PATH = 'E:\\sl2021Project\\twitter sentiment'
+PATH = 'E:\\sl2021Project\\protein data'
 # 写需要存放的文件夹名
 
 
@@ -24,14 +25,17 @@ class Dataset():
         data = pd.read_csv(self.file)
         if(form == 'csvform'):
             return data
+            # 原格式
         data = np.array(data)
         if(form == 'array'):
             return data
+            # nparray格式
         data1 = [l.tolist() for l in data]
         return data1
+        # list格式
 
 
-# 将数据集分为train,validate,test,各占约1/10,约有15~16w行数据,在当前文件夹中存为csv格式
+# 将数据集分为train,validate,test,各占约1/3,在当前文件夹中存为csv格式,无返回值
 def generator(path=PATH, filename='\\all.csv'):
     csv_file = open(path+filename,
                     mode='r', encoding='utf-8')
@@ -39,7 +43,7 @@ def generator(path=PATH, filename='\\all.csv'):
     reader = csv.reader(csv_file)
     trainset, validateset, testset = [], [], []
     for item in reader:
-        a = random.randint(0, 9)
+        a = random.randint(0, 2)
         if(a == 0):
             trainset.append(item)
         if(a == 1):
@@ -64,6 +68,52 @@ def generator(path=PATH, filename='\\all.csv'):
         writer.writerow(i)
     f.close()
 
-# generator()
+# 对蛋白质数据集预处理，仅保留hydro类和trans类数据,并删除除了ID,链条,类型以外所有数据,无返回值
+
+
+def preprocess():
+    csv_file = open('E:\\sl2021Project\\protein data\\pdb_data_no_dups.csv',
+                    mode='r', encoding='utf-8')
+    reader = csv.reader(csv_file)
+    queue_hydro, queue_trans = queue.Queue(), queue.Queue()
+    # 找出哪些ID对应着Hydro和Trans，用queue存放
+    for item in reader:
+        if('HYDRO' in item[1]):
+            queue_hydro.put(item[0])
+        elif('TRANS' in item[1]):
+            queue_trans.put(item[0])
+    csv_file.close()
+    # 找到对应行，并写入新文件all.csv,预处理完成
+    csv_file = open('E:\\sl2021Project\\protein data\\pdb_data_seq.csv',
+                    mode='r', encoding='utf-8')
+    reader = csv.reader(csv_file)
+    f = open(PATH+'\\all.csv', 'w', encoding='utf-8')
+    writer = csv.writer(f)
+    hydro, trans = queue_hydro.get(), queue_trans.get()
+    for item in reader:
+        if(item[0] == hydro):
+            lst = [item[0], item[2], 'hydro']
+            writer.writerow(lst)
+            if(queue_hydro.qsize()):
+                hydro = queue_hydro.get()
+            else:
+                hydro = 'ZZZZZ'
+        elif(item[0] > hydro and queue_hydro.qsize()):
+            hydro = queue_hydro.get()
+        if(item[0] == trans):
+            lst = [item[0], item[2], 'trans']
+            writer.writerow(lst)
+            if(queue_trans.qsize()):
+                trans = queue_trans.get()
+            else:
+                trans = 'ZZZZZ'
+        elif(item[0] > trans and queue_trans.qsize()):
+            trans = queue_trans.get()
+    f.close()
+    csv_file.close()
+
+
+preprocess()
+generator()
 #a = Dataset('train')
 #b = a.Data('list')
